ca851c162b8e196a5ac42a987ec658c42eafc46b
Thiha Soe Htet <thihasoehtet.isn@gmail.com>
2014-05-07 22:50:04 +0800

    Add James Timperley ref

---
+++ DataGridLab/DataGridLab/Controllers/JamesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataGridLab.Controllers
+{
+    public class JamesController : Controller
+    {
+        //
+        // GET: /James/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
